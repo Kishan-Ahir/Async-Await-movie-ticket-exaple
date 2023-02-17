30de99c22cb42666fc938a1b26119681a3b93f6c
Kishan-Ahir <123851397+Kishan-Ahir@users.noreply.github.com>
2023-02-17 08:30:06 +0530

    Async Await movie ticket example

---
+++ movieticketsbyasycn.js
@@ -0,0 +1,100 @@
+
+console.log("person1: shows a tickets")
+console.log("person2: shows a tickets")
+
+
+
+function wifehavetickets(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("wife have a tickets")
+            
+        }, 1000);
+    })
+}
+
+function husbandrequest(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("Now we can go inside.")
+            
+        }, 2000);
+    })
+}
+
+function wiferejects(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("No i am hungry.")
+            
+        }, 3000);
+    })
+}
+function food(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("Husband bought some popcorn for her.")
+            
+        }, 2000);
+    })
+}
+
+function butter(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("wife want's some butter on popcorn.")
+            
+        }, 2000);
+    })
+}
+
+function popcorn_butter(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("husband bought butter for popcorn.")
+            
+        }, 2000);
+    })
+}
+
+function getColdDrinks(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("husband bought some cold drinks also.")
+            
+        },2000)
+    })
+}
+
+
+
+async function trying_something_new (){
+    
+        let bringticks = await wifehavetickets();
+        console.log(bringticks);
+
+        let husbandsreq = await husbandrequest();
+        console.log(husbandsreq);
+
+        let wifesayingno = await wiferejects();
+        console.log(wifesayingno);
+
+        let popcorn = await food();
+        console.log(popcorn);
+
+        let butteronpopcorn = await butter();
+        console.log(butteronpopcorn);
+
+        let popcornwithbutter = await popcorn_butter();
+        console.log(popcornwithbutter);
+
+        let ColdDrinks = await getColdDrinks();
+        console.log(ColdDrinks);
+    
+}
+trying_something_new();
+
+
+
+console.log("person4: shows a tickets")
+console.log("person5: shows a tickets")
